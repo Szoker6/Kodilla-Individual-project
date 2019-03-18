8adeeecbff92731ac20976be7efc2e6a3b4e3d0f
Dawid Margraf <David_neo1@wp.pl>
2019-03-18 23:24:26 +0100

    Delete all

---
+++ js/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/*
+
+(function() {
+  
+   var Menu = (function() {
+     var burger = document.querySelector('.fa');
+     
+     
+     var active = false;
+     
+     var toggleMenu = function() {
+       if (!active) {
+         menu.classList.add('asideBar--active');
+       
+         for (var i = 0, ii = menuItems.length; i < ii; i++) {
+           
+         }
+         
+         active = true;
+       } else {
+         menu.classList.remove('asideBar--active');
+      
+         for (var i = 0, ii = menuItems.length; i < ii; i++) {
+          
+         }
+         
+         active = false;
+       }
+     };
+     
+     var bindActions = function() {
+       burger.addEventListener('click', toggleMenu, false);
+     };
+     
+     var init = function() {
+       bindActions();
+     };
+     
+     return {
+       init: init
+     };
+     
+   }());
+   
+   Menu.init();
+   
+ }());
+
+ */
