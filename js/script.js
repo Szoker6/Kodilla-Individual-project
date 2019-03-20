03d681fffcdb1492e2272e3ff0012366cdd60ef9
Dawid Margraf <David_neo1@wp.pl>
2019-03-21 00:58:28 +0100

    Add hamburger

--- js/script.js
+++ js/script.js
@@ -1,51 +1,12 @@
 'use strict';
 
-/*
+var burgertoggleButton = document.querySelector('.fa-bars');
+var sidebarElement = document.querySelector('.asideBar');
+var generalMarginLeft = document.querySelector('.general');
+var topBarMarginLeft = document.querySelector('.topbar');
 
-(function() {
-  
-   var Menu = (function() {
-     var burger = document.querySelector('.fa');
-     
-     
-     var active = false;
-     
-     var toggleMenu = function() {
-       if (!active) {
-         menu.classList.add('asideBar--active');
-       
-         for (var i = 0, ii = menuItems.length; i < ii; i++) {
-           
-         }
-         
-         active = true;
-       } else {
-         menu.classList.remove('asideBar--active');
-      
-         for (var i = 0, ii = menuItems.length; i < ii; i++) {
-          
-         }
-         
-         active = false;
-       }
-     };
-     
-     var bindActions = function() {
-       burger.addEventListener('click', toggleMenu, false);
-     };
-     
-     var init = function() {
-       bindActions();
-     };
-     
-     return {
-       init: init
-     };
-     
-   }());
-   
-   Menu.init();
-   
- }());
-
- */
+burgertoggleButton.addEventListener('click', function(){
+  sidebarElement.classList.toggle('expanded');
+  generalMarginLeft.classList.toggle('smallGeneralMarginLeft');
+  topBarMarginLeft.classList.toggle('smalltopBarMarginLeft');
+});
