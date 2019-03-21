7af821e9fb39dd41ae014030c00574675fbc264a
Dawid Margraf <David_neo1@wp.pl>
2019-03-21 19:32:57 +0100

    Add marginLeft class

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ var burgertoggleButton = document.querySelector('.fa-bars');
 var sidebarElement = document.querySelector('.asideBar');
 var generalMarginLeft = document.querySelector('.general');
 var topBarMarginLeft = document.querySelector('.topbar');
+var linksMarginLeft = document.querySelector('.links')
 
 burgertoggleButton.addEventListener('click', function(){
   sidebarElement.classList.toggle('expanded');
   generalMarginLeft.classList.toggle('smallGeneralMarginLeft');
   topBarMarginLeft.classList.toggle('smalltopBarMarginLeft');
+  linksMarginLeft.classList.toggle('smallLinksMarginLeft');
 });
